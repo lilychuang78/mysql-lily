78deb433dd26e2938b27b168fdfb385e2c4a4d7f
lilychuang78 <lilychuang78@gmail.com>
2020-11-06 15:31:32 +0000

    python and mysql

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,7 +1,7 @@
 import os
 import pymysql
 
-# Get username from Cloud9 workspace
+# Get the username from the Cloud9 workspace
 # (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
@@ -12,12 +12,11 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
 
 try:
-    # Run a query
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Genre;"
-        cursor.execute(sql)
-        for row in cursor:
-            print(row)
+    with connection.cursor() as cursor:
+        list_of_names = ['fred','Fred']
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings),list_of_names)
+        connection.commit()
+
 finally:
-    # Close the connection, regardless of whether or not the above was successful
     connection.close()
